fb5081144ba2de4f5e10f85b9fdda9c3a75789b9
ghosh99 <ghosh99@yahoo.com>
2013-02-06 16:10:26 -0800

    adding image upload

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,12 @@
 class Listing < ActiveRecord::Base
-  attr_accessible :description, :headline, :location, :user_id
+  attr_accessible :description, :headline, :location, :image, :user_id
 
   belongs_to :user
   
+  mount_uploader :image, ImageUploader
+
   validates :headline, :length => { :maximum => 140 }
+
+  validates :description, :length => { :maximum => 254 }
 
 end
