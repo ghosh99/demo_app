36219e8f4cb5bdc4a5dbd1c202742ce7b3936d68
ghosh99 <ghosh99@yahoo.com>
2013-02-13 19:23:25 -0800

    add profile pic

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,4 +1,5 @@
 class Office < ActiveRecord::Base
   attr_accessible :availability, :company_desc, :company_url, :contact_email, :contact_name, :contact_phone, :contact_picture, :description, :image1, :image2, :image3, :image4, :insurance, :rent, :size, :swap, :title, :years
   mount_uploader :image1, ImageUploader
+  mount_uploader :contact_picture, ImageUploader
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -44,6 +44,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [50, 50]
   end
 
+  version :medium do
+    process :resize_to_limit => [200, 300]
+  end
+
+  version :small do
+    process :resize_to_limit => [100, 100]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
